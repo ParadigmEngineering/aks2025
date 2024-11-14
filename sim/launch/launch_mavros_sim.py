f7180536535d39c4ce40fd0833fa90403be36e4e
ssheppard03 <sfsheppard@mun.ca>
2024-11-14 20:04:42 -0330

    Arming works :)

--- sim/launch/launch_mavros_sim.py
+++ sim/launch/launch_mavros_sim.py
@@ -1,7 +1,12 @@
 #!/usr/bin/env python3
 
 from launch import LaunchDescription
+from launch.actions import ExecuteProcess
 from launch_ros.actions import Node
+import os
+
+full_path = os.path.realpath(__file__)
+path, filename = os.path.split(full_path)
 
 def generate_launch_description():
     return LaunchDescription([
@@ -17,17 +22,10 @@ def generate_launch_description():
                 {'target_system_id': 1},
                 {'target_component_id': 1}
             ]
+        ),
+        ExecuteProcess(
+            cmd=["/bin/bash", path + "/arm_vehicle.sh"],
+            shell=True,
+            output="screen"
         )
-    #     Node(
-    #         package='sim',
-    #         executable='arm_vehicle.py',
-    #         name='arm_vehicle',
-    #         output='screen'
-    #         ),
-    #    Node(
-    #        package='sim',
-    #        executable='set_velocity.py',
-    #        name='set_velocity',
-    #        output='screen',
-    #    )
     ])
