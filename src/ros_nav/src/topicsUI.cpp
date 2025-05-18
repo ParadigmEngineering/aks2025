8e97dcec1cbe9f01fefafbb7776af22ffa6c160e
ssheppard03 <sfsheppard@mun.ca>
2025-05-18 21:24:36 -0230

    Update GUI so we have a map similar to QGC

--- src/ros_nav/src/topicsUI.cpp
+++ src/ros_nav/src/topicsUI.cpp
@@ -7,6 +7,8 @@
 #include "QVBoxLayout"
 #include "QWidget"
 #include "QFont"
+#include "QWebEngineView"
+#include "QWebEnginePage"
 #include "memory"
 #include "thread"
 #include "iomanip"
@@ -31,15 +33,17 @@ public:
 
     void update_gps_ui(const sensor_msgs::msg::NavSatFix& msg) {
         std::ostringstream ss;
-    
+        
         ss << std::fixed << std::setprecision(6) << "Latitude: " << msg.latitude << "°";
         lat_label->setText(QString::fromStdString(ss.str()));
         ss.str("");
-
-        ss << std::fixed << std::setprecision(6) << "Longitutde: " << msg.longitude << "°";
+    
+        ss << std::fixed << std::setprecision(6) << "Longitude: " << msg.longitude << "°";
         lon_label->setText(QString::fromStdString(ss.str()));
         ss.str("");
-
+    
+        QString js = QString("updatePosition(%1, %2);").arg(msg.latitude, 0, 'f', 8).arg(msg.longitude, 0, 'f', 8);
+        map_view->page()->runJavaScript(js);
     }
 
     void update_velo_ui(const geometry_msgs::msg::TwistStamped& msg){
@@ -51,6 +55,10 @@ public:
 
         ss << std::fixed << std::setprecision(2) << "y velocity: " << msg.twist.linear.y << "(m/s)";
         y_velo_label->setText(QString::fromStdString(ss.str()));
+        ss.str("");
+
+        ss << std::fixed << std::setprecision(2) << "velocity: " << std::sqrt(msg.twist.linear.y*msg.twist.linear.y + msg.twist.linear.x*msg.twist.linear.x) << "(m/s)";
+        velo_label->setText(QString::fromStdString(ss.str()));
         ss.str("");
     }
 
@@ -102,9 +110,42 @@ private:
         setup_ui();
     }
 
+    // Map stuff
+    QWebEngineView* map_view;
+    const QString MAP_HTML = R"(
+        <!DOCTYPE html>
+        <html>
+        <head>
+          <meta charset="utf-8">
+          <title>Vehicle Path Map</title>
+          <style>html, body, #map { height: 100%; margin: 0; padding: 0; }</style>
+          <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.3/dist/leaflet.css"/>
+          <script src="https://unpkg.com/leaflet@1.9.3/dist/leaflet.js"></script>
+        </head>
+        <body>
+          <div id="map"></div>
+          <script>
+            var map = L.map('map').setView([0, 0], 18);
+            L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+              maxZoom: 19,
+            }).addTo(map);
+        
+            var latlngs = [];
+            var polyline = L.polyline(latlngs, {color: 'red'}).addTo(map);
+        
+            function updatePosition(lat, lon) {
+              var point = L.latLng(lat, lon);
+              latlngs.push(point);
+              polyline.setLatLngs(latlngs);
+              map.panTo(point);
+            }
+          </script>
+        </body>
+        </html>
+        )";
+        
     void setup_ui() {
         setWindowTitle("MAVROS Monitor");
-        setFixedSize(800, 200);
 
         QGridLayout *layout = new QGridLayout(this);
 
@@ -132,17 +173,23 @@ private:
         lon_label = create_state_label("Longitude: N/A");
         x_velo_label = create_state_label("x velocity: N/A");
         y_velo_label = create_state_label("y velocity: N/A");
+        velo_label = create_state_label("velocity: N/A");
 
         layout->addWidget(title_label, 0, 0);
-        layout->addWidget(gps_label, 0, 2);
-        layout->addWidget(vel_label, 0, 1);
+        layout->addWidget(gps_label, 3, 0);
+        layout->addWidget(vel_label, 6, 0);
         layout->addWidget(armed_label, 1, 0);
         layout->addWidget(mode_label, 2, 0);
-        layout->addWidget(lat_label, 1, 2);
-        layout->addWidget(lon_label, 2, 2);
-        layout->addWidget(x_velo_label, 1, 1);
-        layout->addWidget(y_velo_label, 2, 1);
+        layout->addWidget(lat_label, 4, 0);
+        layout->addWidget(lon_label, 5, 0);
+        layout->addWidget(x_velo_label, 7, 0);
+        layout->addWidget(y_velo_label, 8, 0);
+        layout->addWidget(velo_label, 9, 0);
         
+        map_view = new QWebEngineView(this);
+        map_view->setHtml(MAP_HTML);
+
+        layout->addWidget(map_view, 0, 1, 8, 3);
 
         setLayout(layout);
     }
@@ -172,6 +219,7 @@ private:
 
     QLabel *x_velo_label;
     QLabel *y_velo_label;
+    QLabel *velo_label;
 };
 
 int main(int argc, char *argv[])
@@ -180,7 +228,7 @@ int main(int argc, char *argv[])
     QApplication app(argc, argv);
     
     auto monitor = ROS2StateMonitor::create();
-    monitor->show();
+    monitor->showMaximized();
     
     // Start ROS2 spinner in separate thread
     std::thread ros_thread([monitor]() {
